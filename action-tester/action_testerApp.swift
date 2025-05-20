1e6557531e797d2c6aabf3a777bd4216d0db69c3
Frank Mortensen <fmortens@me.com>
2025-05-20 10:33:02 +0200

    Add code

---
+++ action-tester/action_testerApp.swift
@@ -0,0 +1,17 @@
+//
+//  action_testerApp.swift
+//  action-tester
+//
+//  Created by Frank Aguilar Mortensen on 20/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct action_testerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
